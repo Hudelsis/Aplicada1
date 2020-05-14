7e5aa8e3107abd461c2edcad19a7a3acc7b8facc
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2020-05-15 01:32:57 +0200

    Formulario de Registro de Entreda

--- Aplicada1/MainWindow.xaml.cs
+++ Aplicada1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Aplicada1
+namespace DoControlWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +23,16 @@ namespace Aplicada1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hola Hudelsis", "Aviso", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
